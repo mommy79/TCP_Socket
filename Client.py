eea982d6fb49bc9cfa19c6ee4ed666726c2e9d48
SeoPaul <paul1111@naver.com>
2017-08-16 22:02:05 +0900

    Server class - <Define> Listening Method

--- Client.py
+++ Client.py
@@ -1,10 +1,13 @@
 from socket import *
+from select import *
 
 class Client:
     def __init__(self):
         print("Client class!")
 
-    # TODO: Server에 접속을 요청하는 메소드
+    # TODO: Login요청 메소드
+    def Login_Req(self):
+        socket.connect()
 
     # TODO: Server접속의 성공여부를 알려주는 메소드
 
--- Server.py
+++ Server.py
@@ -1,9 +1,44 @@
 from socket import *
+from select import *
+from Login import *
 
 class Server:
-    # TODO: Server에 접속중인 Client목록을 담는 변수
-    
-    def __init__(self):
-        print("Server class!")
+    # Server의 HOST와 PORT를 담은 변수
+    HOST = '127.0.0.1'
+    PORT = 10000
+    ADDR = (HOST, PORT)
 
-    # TODO: Client가 접속을 요청하면 확인하는 메소드
+    # TODO: Server의 정보와 접속중인 Client정보를 담는 변수
+    __info_list = []
+    __client_info = []
+
+    def __init__(self):
+        self.Listening()
+
+    # TODO: Client가 접속요청을 대기하는 메소드
+    def Listening(self):
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind(Server.ADDR)
+        serverSocket.listen(3)
+
+        Server.__info_list.append(serverSocket)
+        print(Server.__info_list)
+
+        while True:
+            try:
+                read_socket, write_socket, error_socket = select(Server.__info_list,[],[],1)
+
+            except KeyboardInterrupt:
+                serverSocket.close()
+                exit()
+
+            else:
+                # if : serverSocket의 변화 감지시(Login클래스 호출), else : clientSocket의 변화 감지시
+                for sock in read_socket:
+                    if sock == serverSocket:
+                        clientSocket, client_info = serverSocket.accept()
+                        Server.__info_list.append(clientSocket)
+                        Server.__client_info.append(client_info)
+                        l = Login()
+                    else:
+                        continue
